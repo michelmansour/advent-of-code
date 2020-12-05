62a7723e7812bb735fcc9c719b5b6f1d3f77cd0b
Michel Mansour <michel.mansour@gmail.com>
2020-12-05 12:14:06 -0800

    feat: solve 2020 day 1 part 2
    And learned something about ownership and borrowing.

--- 2020/README.md
+++ 2020/README.md
@@ -1,3 +1,8 @@
 # Advent of Code 2020
 
 The Year of Rust
+
+## Day 1
+
+* Part 1: Learned the basics of Rust, like how to print "hello world" and build binaries
+* Part 2: Learned an important bit of the basics I missed in part 1: *ownership*
--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -4,7 +4,8 @@ use std::io::{BufRead, BufReader, Error, ErrorKind};
 
 fn main() {
     println!("Hello, world!");
-    println!("Day 1: {}", day1().unwrap());
+    let (p1, p2) = day1().unwrap();
+    println!("Day 1: p1 {} p2 {}", p1, p2);
 }
 
 fn read_lines(day: i32) -> Result<Vec<String>, Error> {
@@ -12,7 +13,7 @@ fn read_lines(day: i32) -> Result<Vec<String>, Error> {
     return br.lines().collect();
 }
 
-fn day1() -> Result<i32, Error> {
+fn day1() -> Result<(i32, i32), Error> {
     let nums = match read_lines(1)?
         .into_iter()
         .map(|s| s.parse::<i32>())
@@ -20,11 +21,12 @@ fn day1() -> Result<i32, Error> {
             Ok(nums) => nums,
             Err(e) => return Err(Error::new(ErrorKind::InvalidData, e))
         };
-    let (x, y) = two_sum(nums, 2020);
-    return Ok(x * y);
+    let (x, y) = two_sum(&nums, 2020);
+    let (n, p, q) = three_sum(&nums, 2020);
+    return Ok((x * y, n * p * q));
 }
 
-fn two_sum(nums: Vec<i32>, target: i32) -> (i32, i32) {
+fn two_sum(nums: &Vec<i32>, target: i32) -> (i32, i32) {
     let mut complements = HashSet::new();
 
     for n in nums.iter() {
@@ -36,4 +38,17 @@ fn two_sum(nums: Vec<i32>, target: i32) -> (i32, i32) {
         }
     }
     return (-1, 1);
+}
+
+fn three_sum(nums: &Vec<i32>, target: i32) -> (i32, i32, i32) {
+    for n in nums.iter() {
+        for p in nums.iter() {
+            for q in nums.iter() {
+                if n + p + q == target {
+                    return (*n, *p, *q);
+                }
+            }
+        }
+    }
+    return (-1, -1, -1);
 }
